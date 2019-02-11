c8157e40b66ade6774152774505a1cb437adeae5
fuzzylogic2000 <katharina.albers@liqd.de>
2019-02-11 19:05:53 +0100

    add parlamentiary groups (fraktionen)

--- legislatures/admin.py
+++ legislatures/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
 from .models import Legislature
+from .models import ParliamentaryGroup
 
 class LegislatureAdmin(admin.ModelAdmin):
     list_display = (
         '__str__', 'system', 'year', 'start_data', 'percentage'
     )
 
+class ParliamentaryGroupAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__', 'group', 'percentage_women'
+    )
+
 admin.site.register(Legislature, LegislatureAdmin)
+admin.site.register(ParliamentaryGroup, ParliamentaryGroupAdmin)
--- legislatures/models.py
+++ legislatures/models.py
@@ -25,3 +25,33 @@ class Legislature(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ParliamentaryGroup(models.Model):
+    CDU = 'CDU'
+    SPD = 'SPD'
+    FDP = 'FDP'
+    GRUEN = 'GRU'
+    LINKE = 'LIN'
+    AFD = 'AFD'
+    GROUP_CHOICES = (
+        (CDU, 'CDU/CSU'),
+        (SPD, 'SPD'),
+        (FDP, 'FDP'),
+        (GRUEN, 'B\'90/die Grünen, früher: Grüne'),
+        (LINKE, 'Linke, früher: PDS'),
+        (AFD, 'AfD')
+    )
+
+    legislature = models.ForeignKey('Legislature', on_delete=models.CASCADE)
+    group = models.CharField(
+        max_length=3,
+        choices=GROUP_CHOICES
+    )
+    percentage_women = models.DecimalField(decimal_places=2, max_digits=4)
+    number_women = models.IntegerField(blank=True, null=True)
+    percentage_group = models.DecimalField(decimal_places=2, max_digits=4, blank=True, null=True)
+    number_group = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return (self.legislature.name + ' - ' + str(self.legislature.year) + ' - ' + self.get_group_display())
