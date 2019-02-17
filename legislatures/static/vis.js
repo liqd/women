7b2f7e772cfd577d1726de486064b43eb6a249e4
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-02-17 12:56:03 +0100

    add legend

--- legislatures/static/vis.js
+++ legislatures/static/vis.js
@@ -7,7 +7,7 @@ $(function () {
     var formatTime = d3.time.format("%Y")
 
     var x = d3.time.scale()
-        .domain([new Date(1919, 1, 1), new Date(2030, 1, 1)])
+        .domain([new Date(1910, 1, 1), new Date(2030, 1, 1)])
         .range([0, width])
 
     var allValues = values.map(function (d) {
@@ -50,19 +50,19 @@ $(function () {
         .call(yAxis)
 
     svg.append("text")
-      .attr("transform",
-            "translate(" + (width/2) + " ," +
-                           (height + margin.top + 30) + ")")
-      .style("text-anchor", "middle")
-      .text("Jahr");
+        .attr("transform",
+            "translate(" + (width / 2) + " ," +
+            (height + margin.top + 30) + ")")
+        .style("text-anchor", "middle")
+        .text("Jahr");
 
     svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("Frauenanteil in %");
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Frauenanteil in %");
 
     data.forEach(function (item, i) {
         var name = item.name
@@ -73,6 +73,10 @@ $(function () {
         })
 
         svg.append("path")
+            .attr("data-legend", function() {
+                return name
+            })
+            .attr("data-legend-pos", i)
             .attr("class", "line line-" + i)
             .attr("d", valueline(data));
 
@@ -100,6 +104,13 @@ $(function () {
                     .style("opacity", 0);
             });
 
-
     })
+
+    svg.append("g")
+    .attr("class","legend")
+    .attr("transform","translate(50,30)")
+    .attr("data-style-padding",10)
+    .style("font-size","14px")
+    .call(d3.legend)
+
 })
